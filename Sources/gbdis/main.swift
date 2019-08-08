c18beb50f572a65ff84edbbe6d80e6335d0d9fa9
Jeff Verkoeyen <jverkoey@gmail.com>
2019-08-08 03:19:46 +0000

    Progress tracking.

--- Sources/gbdis/main.swift
+++ Sources/gbdis/main.swift
@@ -125,22 +125,4 @@ for bank in UInt8(0)..<UInt8(cpu.numberOfBanks) {
   }
 }
 
-//func disassemble(in bank: UInt64, at pc: UInt16) -> LR35902.Instruction? {
-//  let pcInBank = bank * bankSize + UInt64(pc)
-//  guard let opcode = LR35902.OpCode(rawValue: data[Int(pcInBank)]) else {
-//    return nil
-//  }
-//  return LR35902.Instruction(opcode: opcode)
-//}
-//
-//(0..<numberOfBanks).forEach { bank in
-////  let asmUrl = disassemblyPath.appendingPathComponent("bank_\(bank.hexString).asm")
-//
-////  let bankROMRange = (bank * bankSize)..<((bank + 1) * bankSize)
-//
-//  let pc: UInt16 = 0
-//  if let inst = instruction(in: bank, at: pc) {
-//    let something = data.suffix(from: Int(pc + 1))
-//    print(inst.assemblyRepresentation(something))
-//  }
-//}
+print("code \(Float(cpu.disassembly.code.count) * 100 / Float(cpu.rom.count))%")
