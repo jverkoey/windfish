2196403fc11c61ed9a9175e77b492ad822b6fc16
Jeff Verkoeyen <jverkoey@gmail.com>
2019-08-09 01:53:44 +0000

    Remove unused.

--- Package.swift
+++ Package.swift
@@ -10,13 +10,10 @@ let package = Package(
       name: "gbdis",
       targets: ["gbdis"]),
   ],
-  dependencies: [
-    .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
-  ],
   targets: [
     .target(
       name: "gbdis",
-      dependencies: ["LR35902", "FixedWidthInteger", "SwiftProtobuf"]),
+      dependencies: ["LR35902", "FixedWidthInteger"]),
     .testTarget(
       name: "gbdisTests",
       dependencies: ["gbdis"]),
