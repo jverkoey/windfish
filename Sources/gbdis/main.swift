3a517f12cacb8ce79ba9d5a3a6220385c1823c11
Jeff Verkoeyen <jverkoey@gmail.com>
2019-08-10 18:58:50 +0000

    InitSaves.

--- Sources/gbdis/main.swift
+++ Sources/gbdis/main.swift
@@ -12,6 +12,7 @@ cpu.disassembly.setData(at: 0x0003, in: 0x00)
 
 cpu.disassembly.setLabel(at: 0x0150, in: 0x00, named: "Main")
 cpu.disassembly.setLabel(at: 0x2881, in: 0x00, named: "LCDOff")
+cpu.disassembly.setLabel(at: 0x460F, in: 0x01, named: "InitSaves")
 cpu.disassembly.setPreComment(at: 0x0156, in: 0x00, text: "Reset the palette registers to zero.")
 
 cpu.disassembly.setPreComment(at: 0x015D, in: 0x00, text: "Clears 6144 bytes of video ram. Graphics vram location for OBJ and BG tiles start at $8000 and end at $97FF; for a total of 0x1800 bytes.")
