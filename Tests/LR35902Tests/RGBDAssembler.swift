1f4dae82bca6364c5b2c1517e7860975cabe6609
Jeff Verkoeyen <jverkoey@gmail.com>
2019-08-18 09:58:38 +0000

    Test CB registers.

--- Tests/LR35902Tests/RGBDAssembler.swift
+++ Tests/LR35902Tests/RGBDAssembler.swift
@@ -354,4 +354,18 @@ class RGBDAssembler: XCTestCase {
       0x0000: LR35902.Instruction(spec: .rst(.x38)),
     ])
   }
+
+  func test_rlc_b() throws {
+    let assembler = RGBDSAssembler()
+    let errors = assembler.assemble(assembly: """
+    rlc b
+""")
+    let disassembly = LR35902.Disassembly(rom: assembler.buffer)
+    disassembly.disassemble(range: 0..<UInt16(assembler.buffer.count), inBank: 0x00)
+
+    XCTAssertEqual(errors, [])
+    XCTAssertEqual(disassembly.instructionMap, [
+      0x0000: LR35902.Instruction(spec: .cb(.rlc(.b))),
+    ])
+  }
 }
