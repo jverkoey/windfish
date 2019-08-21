fac0f1bf5a8f52ff6511ba7df6ec24a776bb8b00
Jeff Verkoeyen <jverkoey@gmail.com>
2019-08-21 23:15:17 +0000

    Flatten logic.

--- Sources/LR35902/LR35902+Disassembly+Scope.swift
+++ Sources/LR35902/LR35902+Disassembly+Scope.swift
@@ -16,36 +16,38 @@ extension LR35902.Disassembly {
       }
 
       let runStartAddress = firstRun.startAddress
-      if let runGroupName = labels[runStartAddress] {
-        expandScope(forLabel: runGroupName, scope: scope)
+      guard let runGroupName = labels[runStartAddress] else {
+        continue
+      }
+      expandScope(forLabel: runGroupName, scope: scope)
 
-        // Get the first contiguous block of scope.
-        if let contiguousScope = scope.rangeView.first(where: { $0.lowerBound == runStartAddress }) {
-          for address in contiguousScope {
-            contiguousScopes[LR35902.CartridgeAddress(address)] = runGroupName
+      // Get the first contiguous block of scope.
+      guard let contiguousScope = scope.rangeView.first(where: { $0.lowerBound == runStartAddress }) else {
+        continue
+      }
+      for address in contiguousScope {
+        contiguousScopes[LR35902.CartridgeAddress(address)] = runGroupName
+      }
+
+      var firstReturnIndex: LR35902.CartridgeAddress? = nil
+
+      contiguousScope.dropFirst().forEach {
+        let index = LR35902.CartridgeAddress($0)
+        guard labels[index] != nil else {
+          return
+        }
+        if case .ret = instructionMap[index]?.spec {
+          if let firstReturnIndex = firstReturnIndex {
+            labels[index] = "\(runGroupName).return_\(LR35902.Address(index % LR35902.bankSize).hexString)"
+            labels[firstReturnIndex] = "\(runGroupName).return_\(LR35902.Address(firstReturnIndex % LR35902.bankSize).hexString)"
+          } else {
+            labels[index] = "\(runGroupName).return"
+            firstReturnIndex = index
           }
-
-          var firstReturnIndex: LR35902.CartridgeAddress? = nil
-
-          contiguousScope.dropFirst().forEach {
-            let index = LR35902.CartridgeAddress($0)
-            guard labels[index] != nil else {
-              return
-            }
-            if case .ret = instructionMap[index]?.spec {
-              if let firstReturnIndex = firstReturnIndex {
-                labels[index] = "\(runGroupName).return_\(LR35902.Address(index % LR35902.bankSize).hexString)"
-                labels[firstReturnIndex] = "\(runGroupName).return_\(LR35902.Address(firstReturnIndex % LR35902.bankSize).hexString)"
-              } else {
-                labels[index] = "\(runGroupName).return"
-                firstReturnIndex = index
-              }
-            } else {
-              let bank = LR35902.Bank(index / LR35902.bankSize)
-              let address = index % LR35902.bankSize + LR35902.CartridgeAddress((bank > 0) ? 0x4000 : 0x0000)
-              labels[index] = "\(runGroupName).fn_\(bank.hexString)_\(LR35902.Address(address).hexString)"
-            }
-          }
+        } else {
+          let bank = LR35902.Bank(index / LR35902.bankSize)
+          let address = index % LR35902.bankSize + LR35902.CartridgeAddress((bank > 0) ? 0x4000 : 0x0000)
+          labels[index] = "\(runGroupName).fn_\(bank.hexString)_\(LR35902.Address(address).hexString)"
         }
       }
     }
