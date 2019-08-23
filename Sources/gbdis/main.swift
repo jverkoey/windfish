63a41fdc1e077a55ee6558f1a9b1650e6b7ad3d0
Jeff Verkoeyen <jverkoey@gmail.com>
2019-08-23 17:51:40 -0400

    Pre-comment.

--- Sources/gbdis/main.swift
+++ Sources/gbdis/main.swift
@@ -52,6 +52,7 @@ disassembly.setPreComment(at: 0x0156, in: 0x00, text: "Reset the palette registe
 disassembly.setPreComment(at: 0x015D, in: 0x00, text: "Clears 6144 bytes of video ram. Graphics vram location for OBJ and BG tiles start at $8000 and end at $97FF; for a total of 0x1800 bytes.")
 disassembly.setLabel(at: 0x03bd, in: 0x00, named: "waitForNextFrame")
 disassembly.setLabel(at: 0x01a6, in: 0x00, named: "frameDidRender")
+disassembly.setPreComment(at: 0x01b7, in: 0x00, text: "Load a with a value that is non-zero every other frame.")
 disassembly.setLabel(at: 0x01c4, in: 0x00, named: "setScrollY")
 //disassembly.setLabel(at: 0x01aa, in: 0x00, named: "RenderLoop_setScrollY")
 disassembly.defineFunction(startingAt: 0x07B9, in: 0x00, named: "SetBank")
