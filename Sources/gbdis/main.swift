b8247b929486e5b6fa5bc73186e9d9b8da69d8fd
Jeff Verkoeyen <jverkoey@gmail.com>
2019-08-27 08:55:15 -0400

    More.

--- Sources/gbdis/main.swift
+++ Sources/gbdis/main.swift
@@ -573,6 +573,7 @@ disassembleJumpTable(within: 0x4976..<(0x4976 + 233 * 2), in: 0x03, selectedBank
 disassembleJumpTable(within: 0x5aa6..<0x5ab8, in: 0x03, selectedBank: 0x03)
 disassembleJumpTable(within: 0x5bf5..<0x5bfd, in: 0x03, selectedBank: 0x03)
 disassembleJumpTable(within: 0x5de0..<0x5de6, in: 0x03, selectedBank: 0x03)
+disassembleJumpTable(within: 0x5e43..<0x5e53, in: 0x03, selectedBank: 0x03)
 
 // MARK: - Entity table.
 
