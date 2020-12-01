a60aae918d18bebbb181e16525e9f1e069b3818c
Jeff Verkoeyen <jverkoey@gmail.com>
2020-12-01 13:26:06 -0500

    More storage.

--- ui/gbdis/gbdis/Document.swift
+++ ui/gbdis/gbdis/Document.swift
@@ -9,17 +9,54 @@ import Cocoa
 
 struct MarkdownError: Error { }
 
+struct ProjectMetadata: Codable {
+  var somevalue: String
+}
+
 @objc(MarkdownDocument)
 class MarkdownDocument: NSDocument {
-  private var fileWrapper: FileWrapper?
+  private var documentFileWrapper: FileWrapper?
   let contentViewController = ViewController()
 
-  override func read(from fileWrapper: FileWrapper, ofType typeName: String) throws {
+  private struct Filenames {
+    static let metadata = "metadata.plist"
+  }
 
+  override func read(from fileWrapper: FileWrapper, ofType typeName: String) throws {
+    guard let fileWrappers = fileWrapper.fileWrappers else {
+      preconditionFailure()
+    }
+
+    if let metadataFileWrapper = fileWrappers[Filenames.metadata],
+       let encodedMetadata = metadataFileWrapper.regularFileContents {
+      let decoder = PropertyListDecoder()
+      let metadata = try decoder.decode(ProjectMetadata.self, from: encodedMetadata)
+      Swift.print(metadata)
+    }
   }
 
   override func fileWrapper(ofType typeName: String) throws -> FileWrapper {
-    return FileWrapper()
+    if documentFileWrapper == nil {
+      documentFileWrapper = FileWrapper(directoryWithFileWrappers: [:])
+    }
+    guard let documentFileWrapper = documentFileWrapper else {
+      preconditionFailure()
+    }
+    guard let fileWrappers = documentFileWrapper.fileWrappers else {
+      preconditionFailure()
+    }
+
+    if let metadataFileWrapper = fileWrappers[Filenames.metadata] {
+      documentFileWrapper.removeFileWrapper(metadataFileWrapper)
+    }
+    let metadata = ProjectMetadata(somevalue: "Hello")
+    let encoder = PropertyListEncoder()
+    let encodedMetadata = try encoder.encode(metadata)
+    let metadataFileWrapper = FileWrapper(regularFileWithContents: encodedMetadata)
+    metadataFileWrapper.preferredFilename = Filenames.metadata
+    documentFileWrapper.addFileWrapper(metadataFileWrapper)
+
+    return documentFileWrapper
   }
 
   override func makeWindowControllers() {
