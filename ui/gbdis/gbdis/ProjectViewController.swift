88b7fe4257797be2706e36d831eaada9d1e7b0ff
Jeff Verkoeyen <jverkoey@gmail.com>
2020-12-01 23:55:53 -0500

    Checkpoint.

--- ui/gbdis/gbdis/ProjectViewController.swift
+++ ui/gbdis/gbdis/ProjectViewController.swift
@@ -7,11 +7,287 @@
 
 import Cocoa
 
+func constraints(for contentView: NSView, filling containerView: NSView) -> [NSLayoutConstraint] {
+  return [
+    contentView.leftAnchor.constraint(equalTo: containerView.leftAnchor),
+    contentView.rightAnchor.constraint(equalTo: containerView.rightAnchor),
+    contentView.topAnchor.constraint(equalTo: containerView.topAnchor),
+    contentView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+  ]
+}
+
+enum NodeType: Int, Codable {
+  case container
+  case document
+  case separator
+  case unknown
+}
+
+class OutlineNode: NSObject {
+  var type: NodeType = .unknown
+  var title: String = ""
+  var identifier: String = ""
+  var url: URL?
+  @objc dynamic var children = [OutlineNode]()
+}
+
+extension OutlineNode {
+
+  @objc var count: Int {
+    children.count
+  }
+
+  @objc dynamic var isLeaf: Bool {
+    return type == .document || type == .separator
+  }
+
+  var isURLNode: Bool {
+    return url != nil
+  }
+
+  var canChange: Bool {
+    // You can only change (rename or add to) non-url based directory node.
+    return isDirectory && url == nil
+  }
+
+  var isSeparator: Bool {
+    return type == .separator
+  }
+
+  var isDirectory: Bool {
+    return type == .container
+  }
+
+}
+
+
+extension URL {
+  // Returns the human-visible localized name.
+  var localizedName: String {
+    var localizedName = ""
+    if let fileNameResource = try? resourceValues(forKeys: [.localizedNameKey]) {
+      localizedName = fileNameResource.localizedName!
+    } else {
+      // Failed to get the localized name, use it's last path component as the name.
+      localizedName = lastPathComponent
+    }
+    return localizedName
+  }
+}
+
+final class OutlineViewController: NSViewController {
+  let containerView = NSScrollView()
+  let outlineView = NSOutlineView()
+  let treeController = NSTreeController()
+  @objc var contents: [OutlineNode] = []
+
+  override func loadView() {
+    view = NSView()
+
+    containerView.hasVerticalScroller = true
+    containerView.hasHorizontalScroller = true
+    containerView.autohidesScrollers = true
+    containerView.drawsBackground = false
+    containerView.usesPredominantAxisScrolling = false
+
+    for subview in [containerView] {
+      subview.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview(subview)
+    }
+
+    // Data bindings
+    treeController.objectClass = OutlineNode.self
+    treeController.childrenKeyPath = "children"
+    treeController.countKeyPath = "count"
+    treeController.leafKeyPath = "isLeaf"
+
+    treeController.bind(NSBindingName(rawValue: "contentArray"),
+                        to: self,
+                        withKeyPath: "contents",
+                        options: nil)
+
+    outlineView.bind(NSBindingName(rawValue: "content"),
+                     to: treeController,
+                     withKeyPath: "arrangedObjects",
+                     options: nil)
+
+    let column = NSTableColumn(identifier: .init(rawValue: "col1"))
+    column.isEditable = false
+    column.headerCell.stringValue = "Header"
+    column.resizingMask = .autoresizingMask
+    outlineView.addTableColumn(column)
+    outlineView.outlineTableColumn = column
+
+    outlineView.headerView = nil  // Hide the header
+
+    outlineView.delegate = self
+    outlineView.backgroundColor = .clear
+    outlineView.style = .automatic
+    outlineView.selectionHighlightStyle = .sourceList
+    outlineView.columnAutoresizingStyle = .lastColumnOnlyAutoresizingStyle
+    outlineView.allowsMultipleSelection = false
+    outlineView.allowsEmptySelection = true
+    outlineView.allowsColumnSelection = false
+//    outlineView.autosaveName = "OutlineView"
+    outlineView.autosaveTableColumns = false
+    outlineView.indentationPerLevel = 13
+    outlineView.indentationMarkerFollowsCell = true
+    outlineView.rowSizeStyle = .default
+    outlineView.autosaveExpandedItems = true
+    outlineView.allowsColumnResizing = false
+    outlineView.allowsColumnReordering = false
+    outlineView.floatsGroupRows = false
+
+    NSLayoutConstraint.activate(constraints(for: containerView, filling: view))
+
+    addGroupNode("Top level", identifier: "top-level")
+    let node = OutlineNode()
+    node.title = "Some content"
+    node.type = .document
+    treeController.insert(node, atArrangedObjectIndexPath: IndexPath(indexes: [0, 0]))
+    addGroupNode("Second level", identifier: "second-level")
+
+    outlineView.expandItem(treeController.arrangedObjects.children![0])
+
+    containerView.documentView = outlineView
+  }
+
+  private func addGroupNode(_ folderName: String, identifier: String) {
+    let node = OutlineNode()
+    node.type = .container
+    node.title = folderName
+    node.identifier = identifier
+
+    let insertionIndexPath = IndexPath(index: contents.count)
+    treeController.insert(node, atArrangedObjectIndexPath: insertionIndexPath)
+  }
+
+}
+
+final class TextTableCellView: NSTableCellView {
+  override init(frame frameRect: NSRect) {
+    super.init(frame: frameRect)
+
+    let textField = NSTextField()
+    textField.translatesAutoresizingMaskIntoConstraints = false
+    textField.isBordered = false
+    textField.drawsBackground = false // Required for text color to be set correctly.
+    addSubview(textField)
+
+    self.textField = textField
+
+    NSLayoutConstraint.activate([
+      textField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 3),
+      textField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 3),
+      textField.centerYAnchor.constraint(equalTo: centerYAnchor)
+    ])
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension OutlineViewController: NSOutlineViewDelegate {
+  // Return a Node class from the given outline view item, through it's representedObject.
+  class func node(from item: Any) -> OutlineNode? {
+    if let treeNode = item as? NSTreeNode, let node = treeNode.representedObject as? OutlineNode {
+      return node
+    } else {
+      return nil
+    }
+  }
+
+  func outlineView(_ outlineView: NSOutlineView, isGroupItem item: Any) -> Bool {
+    if let node = OutlineViewController.node(from: item) {
+      return node.type == .container
+    }
+    return false
+  }
+
+  func outlineView(_ outlineView: NSOutlineView, shouldSelectItem item: Any) -> Bool {
+    if let node = OutlineViewController.node(from: item) {
+      return node.type != .container
+    }
+    return true
+  }
+
+  func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
+    guard let node = OutlineViewController.node(from: item) else {
+      return nil
+    }
+
+    let identifier = NSUserInterfaceItemIdentifier("content-cell")
+    var view: NSTableCellView?
+    if let recycledView = outlineView.makeView(withIdentifier: identifier, owner: self) as? NSTableCellView {
+      view = recycledView
+    } else {
+      let newCell = TextTableCellView()
+      newCell.identifier = identifier
+      view = newCell
+    }
+
+    view?.textField?.stringValue = node.title
+    view?.textField?.isEditable = false
+
+    return view
+  }
+}
+
+final class ContentViewController: NSViewController {
+  override func loadView() {
+    view = NSView()
+  }
+}
+
+final class SplitViewController: NSSplitViewController {
+  let sidebarViewController: NSViewController
+  let contentViewController: NSViewController
+
+  override init(nibName nibNameOrNil: NSNib.Name?, bundle nibBundleOrNil: Bundle?) {
+    self.sidebarViewController = OutlineViewController()
+    self.contentViewController = ContentViewController()
+
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+    addSplitViewItem(NSSplitViewItem(sidebarWithViewController: sidebarViewController))
+    addSplitViewItem(NSSplitViewItem(viewController: contentViewController))
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    splitView.dividerStyle = .paneSplitter
+    splitView.isVertical = true
+    splitView.autosaveName = NSSplitView.AutosaveName(splitViewResorationIdentifier)
+    splitView.identifier = NSUserInterfaceItemIdentifier(splitViewResorationIdentifier)
+  }
+
+  private let splitViewResorationIdentifier = "com.featherless.restorationId:SplitViewController"
+}
+
 final class ProjectViewController: NSViewController {
 
   let containerView = NSView()
   let horizontalLine = HorizontalLine()
   let progressIndicator = NSProgressIndicator()
+  let contentViewController: NSViewController
+
+  init() {
+    self.contentViewController = SplitViewController()
+
+    super.init(nibName: nil, bundle: nil)
+
+    self.addChild(self.contentViewController)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 
   public func startProgressIndicator() {
     progressIndicator.isHidden = false
@@ -30,6 +306,9 @@ final class ProjectViewController: NSViewController {
       subview.translatesAutoresizingMaskIntoConstraints = false
       view.addSubview(subview)
     }
+
+    contentViewController.view.translatesAutoresizingMaskIntoConstraints = false
+    containerView.addSubview(contentViewController.view)
 
     progressIndicator.controlSize = .small
     progressIndicator.style = .spinning
@@ -50,6 +329,6 @@ final class ProjectViewController: NSViewController {
       // Progress indicator
       progressIndicator.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
       progressIndicator.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -7)
-    ])
+    ] + constraints(for: contentViewController.view, filling: containerView))
   }
 }
