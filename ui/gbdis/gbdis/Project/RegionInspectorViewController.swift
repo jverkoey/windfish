0c0830f1625022bf05a56f1fe2ec057f447e0983
Jeff Verkoeyen <jverkoey@gmail.com>
2020-12-04 13:12:46 -0500

    Checkpoint.

--- ui/gbdis/gbdis/Project/RegionInspectorViewController.swift
+++ ui/gbdis/gbdis/Project/RegionInspectorViewController.swift
@@ -7,10 +7,51 @@
 
 import Foundation
 import Cocoa
+import LR35902
+
+class Region: NSObject, NSCopying {
+  @objc dynamic let name: String
+  @objc dynamic let bank: LR35902.Bank
+  @objc dynamic let address: LR35902.Address
+  @objc dynamic let length: LR35902.Address
+
+  init(name: String, bank: LR35902.Bank, address: LR35902.Address, length: LR35902.Address) {
+    self.name = name
+    self.bank = bank
+    self.address = address
+    self.length = length
+  }
+
+  func copy(with zone: NSZone? = nil) -> Any {
+    return Region(name: name, bank: bank, address: address, length: length)
+  }
+}
+
+final class LR35902AddressFormatter: Formatter {
+  override func string(for obj: Any?) -> String? {
+    guard let address = obj as? LR35902.Address else {
+      return nil
+    }
+    return "0x\(address.hexString)"
+  }
+//
+//  override func getObjectValue(_ obj: AutoreleasingUnsafeMutablePointer<AnyObject?>?,
+//                               for string: String,
+//                               errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
+//    if string.hasPrefix("0x") {
+//      let numericalValue = string.dropFirst(3)
+//      obj?.pointee = LR35902.Address(numericalValue, radix: 16)
+//      return true
+//    }
+//    return false
+//  }
+}
 
 final class RegionInspectorViewController: NSViewController, TabSelectable {
   let deselectedTabImage = NSImage(systemSymbolName: "book", accessibilityDescription: nil)!
   let selectedTabImage = NSImage(systemSymbolName: "book.fill", accessibilityDescription: nil)!
+
+  let regionController = NSArrayController()
 
   override func loadView() {
     view = NSView()
@@ -21,8 +62,9 @@ final class RegionInspectorViewController: NSViewController, TabSelectable {
 
     let regionTableView = NSTableView()
     regionTableView.translatesAutoresizingMaskIntoConstraints = false
-    regionTableView.dataSource = self
     regionTableView.delegate = self
+    regionTableView.style = .fullWidth
+    regionTableView.selectionHighlightStyle = .regular
     containerView.documentView = regionTableView
     view.addSubview(containerView)
 
@@ -33,53 +75,67 @@ final class RegionInspectorViewController: NSViewController, TabSelectable {
       containerView.heightAnchor.constraint(equalToConstant: 200)
     ])
 
-    for columnName in ["Name", "Bank", "Start address", "Length"] {
-      let column = NSTableColumn(identifier: .init(rawValue: columnName))
+    for columnName in [("Name", "name"), ("Bank", "bank"), ("Address", "address"), ("Length", "length")] {
+      let column = NSTableColumn(identifier: .init(rawValue: columnName.1))
       column.isEditable = false
-      column.headerCell.stringValue = columnName
+      column.headerCell.stringValue = columnName.0
+      column.width = 50
+      // Note: this only works for cell-based tables.
+//      column.bind(.value, to: regionController, withKeyPath: "arrangedObjects.name", options: nil)
       regionTableView.addTableColumn(column)
     }
-  }
-}
 
-extension RegionInspectorViewController: NSTableViewDataSource {
-  func numberOfRows(in tableView: NSTableView) -> Int {
-    return 10
+    regionController.addObject(Region(
+      name: "RST_00",
+      bank: 0,
+      address: 0x0000,
+      length: 8
+    ))
+
+    regionTableView.bind(.content, to: regionController, withKeyPath: "arrangedObjects", options: nil)
+    regionTableView.bind(.selectionIndexes, to: regionController, withKeyPath:"selectionIndexes", options: nil)
+    regionTableView.bind(.sortDescriptors, to: regionController, withKeyPath: "sortDescriptors", options: nil)
   }
 }
 
 extension RegionInspectorViewController: NSTableViewDelegate {
-  func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-    let textField: NSTextField
+  func tableView(_ tableView: NSTableView, rowViewForRow row: Int) -> NSTableRowView? {
+    let rowView = NSTableRowView()
+    return rowView
+  }
 
-    switch tableColumn?.identifier {
-    case .init("Name"):
+  func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+    guard let tableColumn = tableColumn else {
+      preconditionFailure()
+    }
+    let view: TextTableCellView
+
+    switch tableColumn.identifier {
+    case .init("name"): fallthrough
+    case .init("bank"): fallthrough
+    case .init("length"):
       let identifier = NSUserInterfaceItemIdentifier("textCell")
-      if let existingTextField = tableView.makeView(withIdentifier: identifier, owner: self) as? NSTextField {
-        textField = existingTextField
+      if let recycledView = tableView.makeView(withIdentifier: identifier, owner: self) as? TextTableCellView {
+        view = recycledView
       } else {
-        textField = NSTextField()
-        textField.identifier = identifier
-        textField.drawsBackground = false
+        view = TextTableCellView()
+        view.identifier = identifier
       }
-      textField.stringValue = "Hello"
-    case .init("Bank"): fallthrough
-    case .init("Start address"): fallthrough
-    case .init("Length"):
+    case .init("address"):
       let identifier = NSUserInterfaceItemIdentifier("numberCell")
-      if let existingTextField = tableView.makeView(withIdentifier: identifier, owner: self) as? NSTextField {
-        textField = existingTextField
+      if let recycledView = tableView.makeView(withIdentifier: identifier, owner: self) as? TextTableCellView {
+        view = recycledView
       } else {
-        textField = NSTextField()
-        textField.identifier = identifier
-        textField.drawsBackground = false
+        view = TextTableCellView()
+        view.identifier = identifier
+        view.textField?.formatter = LR35902AddressFormatter()
       }
-      textField.stringValue = "123"
     default:
       preconditionFailure()
     }
 
+    view.textField?.bind(.value, to: view, withKeyPath: "objectValue.\(tableColumn.identifier.rawValue)", options: nil)
 
-    return textField
+    return view
   }
 }
