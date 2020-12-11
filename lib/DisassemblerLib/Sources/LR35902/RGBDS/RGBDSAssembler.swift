ed5feaa821280971b841405a28ffd51812926fc8
Jeff Verkoeyen <jverkoey@gmail.com>
2020-12-10 23:40:21 -0500

    Moving files around.

--- lib/DisassemblerLib/Sources/LR35902/RGBDS/RGBDSAssembler.swift
+++ lib/DisassemblerLib/Sources/LR35902/RGBDS/RGBDSAssembler.swift
@@ -4,54 +4,6 @@ import CPU
 import FoundationExtensions
 import RGBDS
 
-extension LR35902.InstructionSet {
-  /**
-   A cached map of specifications to their tokenized representation.
-
-   This is typically implemented by returning the result of `computeAllOpcodeStrings()`.
-   */
-  static var specToTokenString: [SpecType: String] = {
-    return computeAllTokenStrings()
-  }()
-
-  /**
-   A cached map of specifications to their tokenized representation.
-
-   This is typically implemented by returning the result of `computeAllOpcodeStrings()`.
-   */
-  static var tokenStringToSpecs: [String: [SpecType]] = {
-    return .init(specToTokenString.map { ($0.value, [$0.key]) }, uniquingKeysWith: +)
-  }()
-
-  private static func computeAllTokenStrings() -> [SpecType: String] {
-    return allSpecs().reduce(into: [:]) { accumulator, spec in
-      var operands: [String] = []
-      spec.visit { operand in
-        // Optional operands are provided by the visitor as boxed optional types represented as an Any.
-        // We can't cast an Any to an Any? using the as? operator, so perform an explicit Optional-type unboxing instead:
-        guard let valueUnboxed = operand?.value,
-              case Optional<Any>.some(let value) = valueUnboxed else {
-          return
-        }
-
-        if let representable = value as? InstructionOperandTokenizable {
-          operands.append(representable.token.asString())
-        } else {
-          operands.append("\(value)")
-        }
-      }
-      guard let opcode = opcodeStrings[spec] else {
-        return
-      }
-      var representationParts = [opcode]
-      if !operands.isEmpty {
-        representationParts.append(operands.joined(separator: ", "))
-      }
-      accumulator[spec] = representationParts.joined(separator: " ")
-    }
-  }
-}
-
 private func createStatement(from code: String) -> RGBDSAssembly.Statement {
   let opcodeAndOperands = code.split(separator: " ", maxSplits: 1)
 
@@ -89,8 +41,8 @@ private func createRepresentation(from statement: RGBDSAssembly.Statement) -> St
 }
 
 private func cast<T: UnsignedInteger, negT: SignedInteger>(string: String, negativeType: negT.Type)
-  throws -> T
-  where T: FixedWidthInteger, negT: FixedWidthInteger, T: BitPatternInitializable, T.CompanionType == negT {
+throws -> T
+where T: FixedWidthInteger, negT: FixedWidthInteger, T: BitPatternInitializable, T.CompanionType == negT {
   var value = string
   let isNegative = value.starts(with: "-")
   if isNegative {
---
+++ lib/DisassemblerLib/Sources/LR35902/RGBDS/Tokenization.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+import RGBDS
+
+extension LR35902.InstructionSet {
+  /**
+   A cached map of specifications to their tokenized representation.
+
+   This is typically implemented by returning the result of `computeAllOpcodeStrings()`.
+   */
+  static var specToTokenString: [SpecType: String] = {
+    return computeAllTokenStrings()
+  }()
+
+  /**
+   A cached map of specifications to their tokenized representation.
+
+   This is typically implemented by returning the result of `computeAllOpcodeStrings()`.
+   */
+  static var tokenStringToSpecs: [String: [SpecType]] = {
+    return .init(specToTokenString.map { ($0.value, [$0.key]) }, uniquingKeysWith: +)
+  }()
+
+  private static func computeAllTokenStrings() -> [SpecType: String] {
+    return allSpecs().reduce(into: [:]) { accumulator, spec in
+      var operands: [String] = []
+      spec.visit { operand in
+        // Optional operands are provided by the visitor as boxed optional types represented as an Any.
+        // We can't cast an Any to an Any? using the as? operator, so perform an explicit Optional-type unboxing instead:
+        guard let valueUnboxed = operand?.value,
+              case Optional<Any>.some(let value) = valueUnboxed else {
+          return
+        }
+
+        if let representable = value as? InstructionOperandTokenizable {
+          operands.append(representable.token.asString())
+        } else {
+          operands.append("\(value)")
+        }
+      }
+      guard let opcode = opcodeStrings[spec] else {
+        return
+      }
+      var representationParts = [opcode]
+      if !operands.isEmpty {
+        representationParts.append(operands.joined(separator: ", "))
+      }
+      accumulator[spec] = representationParts.joined(separator: " ")
+    }
+  }
+}
