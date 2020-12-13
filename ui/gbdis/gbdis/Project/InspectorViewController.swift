090b6571c4cebf358cf625dc066d3ad03d4ae79e
Jeff Verkoeyen <jverkoey@gmail.com>
2020-12-12 21:41:56 -0500

    Add a macro editor view controller.

--- ui/gbdis/gbdis/Project/InspectorViewController.swift
+++ ui/gbdis/gbdis/Project/InspectorViewController.swift
@@ -45,6 +45,7 @@ final class InspectorViewController: NSViewController {
     editorTabViewController.tabViewController.addTabViewItem(NSTabViewItem(viewController: RegionEditorViewController(document: document)))
     editorTabViewController.tabViewController.addTabViewItem(NSTabViewItem(viewController: DataTypeEditorViewController(document: document)))
     editorTabViewController.tabViewController.addTabViewItem(NSTabViewItem(viewController: GlobalEditorViewController(document: document)))
+    editorTabViewController.tabViewController.addTabViewItem(NSTabViewItem(viewController: MacroEditorViewController(document: document)))
     tabViewController.tabViewController.addTabViewItem(NSTabViewItem(viewController: editorTabViewController))
     tabViewController.tabViewController.addTabViewItem(NSTabViewItem(viewController: RegionInspectorViewController(document: document)))
 
