fc984130da0795973e17244f7a73a9e20bb76811
Jeff Verkoeyen <jverkoey@gmail.com>
2020-12-22 00:28:14 -0500

    Sanity check memory mapping.

--- lib/Sources/Windfish/Hardware/Memory/MainMemory.swift
+++ lib/Sources/Windfish/Hardware/Memory/MainMemory.swift
@@ -13,14 +13,14 @@ extension Gameboy {
     }
 
     private var mappedRegions: [ClosedRange<LR35902.Address>: AddressableMemory] = [:]
+    private var mappedBytes = IndexSet()
     private var hram = GenericRAM(addressableRanges: [0xFF80...0xFFFE])
 
     public func read(from address: LR35902.Address) -> UInt8 {
       if let memory = mappedRegions.first(where: { range, _ in range.contains(address) })?.value {
         return memory.read(from: address)
       }
-      preconditionFailure("No region mapped to this address.")
-      return 0xff
+      fatalError("No region mapped to this address.")
     }
 
     public mutating func write(_ byte: UInt8, to address: LR35902.Address) {
@@ -29,14 +29,17 @@ extension Gameboy {
         mappedRegions[mappedRegion.key] = mappedRegion.value
         return
       }
-
-      preconditionFailure("No region mapped to this address.")
+      fatalError("No region mapped to this address.")
     }
 
     // MARK: - Mapping regions of memory
 
     private mutating func mapRegion(to memory: AddressableMemory) {
       for range in memory.addressableRanges {
+        let intRange = Int(range.lowerBound)...Int(range.upperBound)
+        precondition(!mappedBytes.contains(integersIn: intRange), "Memory is already mapped to this region.")
+        mappedBytes.insert(integersIn: intRange)
+
         mappedRegions[range] = memory
       }
     }
