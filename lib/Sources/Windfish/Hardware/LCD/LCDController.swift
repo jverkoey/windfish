79af46385968e7ccfa790d9643955b24820ef657
Jeff Verkoeyen <jverkoey@gmail.com>
2020-12-26 00:22:16 -0500

    Implement LCDC STAT.

--- lib/Sources/Windfish/Hardware/LCD/LCDController.swift
+++ lib/Sources/Windfish/Hardware/LCD/LCDController.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+// References:
+// https://www.youtube.com/watch?v=HyzD8pNlpwI&t=29m19s
+// https://gbdev.gg8.se/wiki/articles/Video_Display#FF41_-_STAT_-_LCDC_Status_.28R.2FW.29
+// https://realboyemulator.files.wordpress.com/2013/01/gbcpuman.pdf
+
 public struct LCDController {
   public let addressableRanges: [ClosedRange<LR35902.Address>] = [
     0xFF40...0xFF45
@@ -14,10 +19,8 @@ public struct LCDController {
     case LYC  = 0xFF45
   }
   var values: [Addresses: UInt8] = [
-    .STAT: 0x00,
     .SCY:  0x00,
     .SCX:  0x00,
-    .LYC:  0x00,
   ]
 
   // MARK: LCDC bits
@@ -56,9 +59,44 @@ public struct LCDController {
   var objEnable = false                                // bit 1
   var backgroundEnable = true                          // bit 0
 
+  // MARK: STAT bits
+
+  enum LCDCMode {
+    case hblank
+    case vblank
+
+    // TODO: Not able to read oamram during this mode
+    case searchingOAM
+
+    // TODO: Any reads of vram or oamram during this mode should return 0xff; writes are ignored
+    case transferringToLCDDriver
+
+    var bits: UInt8 {
+      switch self {
+      case .hblank:                   return 0b0000_0000
+      case .vblank:                   return 0b0000_0001
+      case .searchingOAM:             return 0b0000_0010
+      case .transferringToLCDDriver:  return 0b0000_0011
+      }
+    }
+  }
+  var enableCoincidenceInterrupt = false  // bit 6
+  var enableOAMInterrupt = false          // bit 5
+  var enableVBlankInterrupt = false       // bit 4
+  var enableHBlankInterrupt = false       // bit 3
+  var coincidence: Bool {                 // bit 2
+    return ly == lyc
+  }
+  var lcdMode = LCDCMode.hblank           // bits 1 and 0
+
   // MARK: LY
 
+  /** The vertical line to which data is transferred to the display. */
   var ly: UInt8 = 0
+
+  // MARK: LYC
+
+  var lyc: UInt8 = 0
 }
 
 extension LCDController: AddressableMemory {
@@ -78,6 +116,20 @@ extension LCDController: AddressableMemory {
           | (objEnable                          ? 0b0000_0010 : 0)
           | (backgroundEnable                   ? 0b0000_0001 : 0)
       )
+
+    case .LY:   return ly
+    case .LYC:  return lyc
+
+    case .STAT:
+      return (
+        (enableCoincidenceInterrupt   ? 0b0100_0000 : 0)
+          | (enableOAMInterrupt       ? 0b0010_0000 : 0)
+          | (enableVBlankInterrupt    ? 0b0001_0000 : 0)
+          | (enableHBlankInterrupt    ? 0b0000_1000 : 0)
+          | (coincidence              ? 0b0000_0100 : 0)
+          | lcdMode.bits
+      )
+
     default:
       return values[lcdAddress]!
     }
@@ -97,6 +149,16 @@ extension LCDController: AddressableMemory {
       spriteSize                = (byte & 0b0000_0100) > 0 ? .x8x16 : .x8x8
       objEnable                 = (byte & 0b0000_0010) > 0
       backgroundEnable          = (byte & 0b0000_0001) > 0
+
+    case .LY:  ly = 0
+    case .LYC: lyc = 0
+
+    case .STAT:
+      enableCoincidenceInterrupt  = (byte & 0b0100_0000) > 0
+      enableOAMInterrupt          = (byte & 0b0010_0000) > 0
+      enableVBlankInterrupt       = (byte & 0b0001_0000) > 0
+      enableHBlankInterrupt       = (byte & 0b0000_1000) > 0
+
     default:
       values[lcdAddress] = byte
     }
--- lib/Tests/WindfishTests/Emulator/LCDControllerTests.swift
+++ lib/Tests/WindfishTests/Emulator/LCDControllerTests.swift
@@ -5,6 +5,9 @@ class LCDControllerTests: XCTestCase {
   func testInitialState() {
     let controller = LCDController()
 
-    XCTAssertEqual(controller.read(from: LCDController.Addresses.LCDC.rawValue), 0x91)
+    XCTAssertEqual(controller.read(from: LCDController.Addresses.LCDC.rawValue), 0b1001_0001)
+    XCTAssertEqual(controller.read(from: LCDController.Addresses.STAT.rawValue), 0b0000_0100)
+    XCTAssertEqual(controller.ly, 0)
+    XCTAssertEqual(controller.lyc, 0)
   }
 }
