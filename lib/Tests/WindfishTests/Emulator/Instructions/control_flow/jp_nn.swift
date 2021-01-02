1cb85e39d1d39ed01802de1164e47e1600be4f45
Jeff Verkoeyen <jverkoey@gmail.com>
2021-01-02 00:41:23 -0500

    Test jp hl

--- lib/Tests/WindfishTests/Emulator/Instructions/control_flow/jp_nn.swift
+++ lib/Tests/WindfishTests/Emulator/Instructions/control_flow/jp_nn.swift
@@ -22,4 +22,25 @@ extension InstructionEmulatorTests {
       assertEqual(cpu, mutations, message: "Test case: \(name)")
     }
   }
+
+  func test_jp_hl() {
+    for spec in LR35902.InstructionSet.allSpecs() {
+      guard case .jp(nil, .hl) = spec, let emulator = LR35902.Emulation.jp_hl(spec: spec) else { continue }
+      InstructionEmulatorTests.testedSpecs.insert(spec)
+      let memory = TestMemory(defaultReadValue: 0x12)
+
+      let cpu = LR35902.zeroed()
+      cpu.hl = 0x1234
+      let mutations = cpu.copy()
+
+      var cycle = 0
+      repeat {
+        cycle += 1
+      } while emulator.advance(cpu: cpu, memory: memory, cycle: cycle, sourceLocation: .memory(0)) == .continueExecution
+
+      XCTAssertEqual(cycle, 1, "Test case: \(name)")
+      mutations.pc = 0x1234
+      assertEqual(cpu, mutations, message: "Test case: \(name)")
+    }
+  }
 }
