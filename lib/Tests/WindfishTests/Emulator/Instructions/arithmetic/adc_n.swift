7bc799ba7dcff9f22d11cb98dfd8de42d7938d6d
Jeff Verkoeyen <jverkoey@gmail.com>
2021-01-03 19:17:17 -0500

    Test adc_hladdr

--- lib/Tests/WindfishTests/Emulator/Instructions/arithmetic/adc_n.swift
+++ lib/Tests/WindfishTests/Emulator/Instructions/arithmetic/adc_n.swift
@@ -13,9 +13,8 @@ extension InstructionEmulatorTests {
       let a: UInt8
     }
     let result: Result
-  }
-  func test_adc_n() {
-    let testCases: [String: TestCase] = [
+
+    static let testCases: [String: TestCase] = [
       "zero":            .init(a: 0,    imm8: 0,    fc: false, result: .init(fz: true,  fc: false, fh: false, a: 0)),
       "positive":        .init(a: 0,    imm8: 1,    fc: false, result: .init(fz: false, fc: false, fh: false, a: 1)),
       "overflowzero":    .init(a: 0xff, imm8: 1,    fc: false, result: .init(fz: true,  fc: true,  fh: true,  a: 0)),
@@ -30,8 +29,10 @@ extension InstructionEmulatorTests {
       "chalfcarry":       .init(a: 0x0f, imm8: 1,    fc: true, result: .init(fz: false, fc: false, fh: true,  a: 0x11)),
       "chalfcarry2":      .init(a: 0x0f, imm8: 0x11, fc: true, result: .init(fz: false, fc: false, fh: true,  a: 0x21)),
     ]
+  }
 
-    for (name, testCase) in testCases {
+  func test_adc_n() {
+    for (name, testCase) in TestCase.testCases {
       for spec in LR35902.InstructionSet.allSpecs() {
         guard let emulator = LR35902.Emulation.adc_n(spec: spec) else { continue }
         InstructionEmulatorTests.testedSpecs.insert(spec)
@@ -48,6 +49,32 @@ extension InstructionEmulatorTests {
 
         XCTAssertEqual(cycle, 2, "Test case: \(name)")
         mutations.pc += 1
+        mutations.a = testCase.result.a
+        mutations.fzero = testCase.result.fz
+        mutations.fcarry = testCase.result.fc
+        mutations.fhalfcarry = testCase.result.fh
+        assertEqual(cpu, mutations, message: "Test case: \(name)")
+      }
+    }
+  }
+
+  func test_adc_hladdr() {
+    for (name, testCase) in TestCase.testCases {
+      for spec in LR35902.InstructionSet.allSpecs() {
+        guard let emulator = LR35902.Emulation.adc_hladdr(spec: spec) else { continue }
+        InstructionEmulatorTests.testedSpecs.insert(spec)
+        let memory = TestMemory(defaultReadValue: testCase.imm8)
+        let cpu = LR35902.zeroed()
+        cpu.a = testCase.a
+        cpu.fcarry = testCase.fc
+        let mutations = cpu.copy()
+
+        var cycle = 0
+        repeat {
+          cycle += 1
+        } while emulator.advance(cpu: cpu, memory: memory, cycle: cycle, sourceLocation: .memory(0)) == .continueExecution
+
+        XCTAssertEqual(cycle, 2, "Test case: \(name)")
         mutations.a = testCase.result.a
         mutations.fzero = testCase.result.fz
         mutations.fcarry = testCase.result.fc
