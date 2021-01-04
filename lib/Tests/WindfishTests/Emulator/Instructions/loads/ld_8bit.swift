a6adc5b68fe63bef29e6a7a4d549183e508826e9
Jeff Verkoeyen <jverkoey@gmail.com>
2021-01-03 21:12:41 -0500

    Test ldd

--- lib/Tests/WindfishTests/Emulator/Instructions/loads/ld_8bit.swift
+++ lib/Tests/WindfishTests/Emulator/Instructions/loads/ld_8bit.swift
@@ -264,4 +264,55 @@ extension InstructionEmulatorTests {
       ], "\(spec)")
     }
   }
+
+  func test_ldd_a_hladdr() {
+    for spec in LR35902.InstructionSet.allSpecs() {
+      guard let emulator = LR35902.Emulation.ldd_a_hladdr(spec: spec) else { continue }
+      InstructionEmulatorTests.testedSpecs.insert(spec)
+      let memory = TestMemory(defaultReadValue: 0x12)
+
+      let cpu = LR35902.zeroed()
+      cpu.hl = 0x4567
+      let mutations = cpu.copy()
+      mutations.hl = 0x4566
+      mutations.a = 0x12
+
+      var cycle = 0
+      repeat {
+        cycle += 1
+      } while emulator.advance(cpu: cpu, memory: memory, cycle: cycle, sourceLocation: .memory(0)) == .continueExecution
+
+      XCTAssertEqual(cycle, 2, "\(spec)")
+      assertEqual(cpu, mutations, message: "\(spec)")
+      XCTAssertEqual(memory.reads, [0x4567], "\(spec)")
+      XCTAssertEqual(memory.writes, [])
+    }
+  }
+
+  func test_ldd_hladdr_a() {
+    for spec in LR35902.InstructionSet.allSpecs() {
+      guard let emulator = LR35902.Emulation.ldd_hladdr_a(spec: spec) else { continue }
+      InstructionEmulatorTests.testedSpecs.insert(spec)
+      let memory = TestMemory()
+
+      let cpu = LR35902.zeroed()
+      cpu.a = 0x12
+      cpu.hl = 0x4567
+      let mutations = cpu.copy()
+      mutations.hl = 0x4566
+
+      var cycle = 0
+      repeat {
+        cycle += 1
+      } while emulator.advance(cpu: cpu, memory: memory, cycle: cycle, sourceLocation: .memory(0)) == .continueExecution
+
+      XCTAssertEqual(cycle, 2, "\(spec)")
+      assertEqual(cpu, mutations, message: "\(spec)")
+      XCTAssertEqual(memory.reads, [], "\(spec)")
+      XCTAssertEqual(memory.writes, [
+        .init(byte: 0x12, address: 0x4567)
+      ])
+    }
+  }
+
 }
