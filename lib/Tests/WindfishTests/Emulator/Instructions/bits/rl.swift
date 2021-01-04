2351f0739de61d4aca753ea58dfe22f95ef8c383
Jeff Verkoeyen <jverkoey@gmail.com>
2021-01-03 20:31:34 -0500

    Test rl hladdr

--- lib/Tests/WindfishTests/Emulator/Instructions/bits/rl.swift
+++ lib/Tests/WindfishTests/Emulator/Instructions/bits/rl.swift
@@ -53,4 +53,37 @@ extension InstructionEmulatorTests {
       }
     }
   }
+
+  func test_rl_hladdr() {
+    for spec in LR35902.InstructionSet.allSpecs() {
+      guard let emulator = LR35902.Emulation.rl_hladdr(spec: spec) else { continue }
+      InstructionEmulatorTests.testedSpecs.insert(spec)
+      for (name, testCase) in TestCase.testCases {
+        let memory = TestMemory(defaultReadValue: testCase.value)
+        let cpu = LR35902.zeroed()
+        cpu.hl = 0x1234
+        cpu.fsubtract = true
+        cpu.fhalfcarry = true
+        cpu.fzero = !testCase.result.fz
+        cpu.fcarry = testCase.fc
+        let mutations = cpu.copy()
+
+        var cycle = 0
+        repeat {
+          cycle += 1
+        } while emulator.advance(cpu: cpu, memory: memory, cycle: cycle, sourceLocation: .memory(0)) == .continueExecution
+
+        XCTAssertEqual(cycle, 4, "Test case: \(name)")
+        mutations.fcarry = testCase.result.fc
+        mutations.fzero = testCase.result.fz
+        mutations.fsubtract = false
+        mutations.fhalfcarry = false
+        assertEqual(cpu, mutations, message: "Test case: \(name)")
+        XCTAssertEqual(memory.reads, [0x1234], "\(name)")
+        XCTAssertEqual(memory.writes, [
+          .init(byte: testCase.result.value, address: 0x1234),
+        ], "\(name)")
+      }
+    }
+  }
 }
