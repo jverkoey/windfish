7fdcdf649f7fc35069e725369d5bd75fea6eedd6
Jeff Verkoeyen <jverkoey@gmail.com>
2021-01-03 20:50:30 -0500

    Test swap hladdr.

--- lib/Tests/WindfishTests/Emulator/Instructions/bits/swap.swift
+++ lib/Tests/WindfishTests/Emulator/Instructions/bits/swap.swift
@@ -19,11 +19,10 @@ extension InstructionEmulatorTests {
   }
 
   func test_swap_r() {
-    for (name, testCase) in TestCase.testCases {
-      for spec in LR35902.InstructionSet.allSpecs() {
-        guard case .cb(.swap(let register)) = spec,
-              let emulator = LR35902.Emulation.swap_r(spec: spec) else { continue }
-        InstructionEmulatorTests.testedSpecs.insert(spec)
+    for spec in LR35902.InstructionSet.allSpecs() {
+      guard case .cb(.swap(let register)) = spec, let emulator = LR35902.Emulation.swap_r(spec: spec) else { continue }
+      InstructionEmulatorTests.testedSpecs.insert(spec)
+      for (name, testCase) in TestCase.testCases {
         let memory = TestMemory()
         let cpu = LR35902.zeroed()
         cpu[register] = testCase.value
@@ -45,6 +44,39 @@ extension InstructionEmulatorTests {
         mutations.fhalfcarry = false
         mutations[register] = testCase.result.value
         assertEqual(cpu, mutations, message: "Test case: \(name)")
+      }
+    }
+  }
+
+  func test_swap_hladdr() {
+    for spec in LR35902.InstructionSet.allSpecs() {
+      guard let emulator = LR35902.Emulation.swap_hladdr(spec: spec) else { continue }
+      InstructionEmulatorTests.testedSpecs.insert(spec)
+      for (name, testCase) in TestCase.testCases {
+        let memory = TestMemory(defaultReadValue: testCase.value)
+        let cpu = LR35902.zeroed()
+        cpu.hl = 0x1234
+        cpu.fsubtract = true
+        cpu.fhalfcarry = true
+        cpu.fzero = !testCase.result.fz
+        cpu.fcarry = true
+        let mutations = cpu.copy()
+
+        var cycle = 0
+        repeat {
+          cycle += 1
+        } while emulator.advance(cpu: cpu, memory: memory, cycle: cycle, sourceLocation: .memory(0)) == .continueExecution
+
+        XCTAssertEqual(cycle, 4, "Test case: \(name)")
+        mutations.fcarry = false
+        mutations.fzero = testCase.result.fz
+        mutations.fsubtract = false
+        mutations.fhalfcarry = false
+        assertEqual(cpu, mutations, message: "Test case: \(name)")
+        XCTAssertEqual(memory.reads, [0x1234], "\(name)")
+        XCTAssertEqual(memory.writes, [
+          .init(byte: testCase.result.value, address: 0x1234),
+        ], "\(name)")
       }
     }
   }
