75f02bc85169ece5167fd46549fe6141d0eaa695
Jeff Verkoeyen <jverkoey@gmail.com>
2021-01-14 01:03:42 -0500

    Implement boot loading.

--- ui/KoholintIsland/Project/Model/ProjectDocument+Sameboy.swift
+++ ui/KoholintIsland/Project/Model/ProjectDocument+Sameboy.swift
@@ -21,6 +21,16 @@ extension ProjectDocument: EmulatorDelegate {
   // MARK: - CallbackBridgeDelegate
 
   func loadBootROM(_ type: GB_boot_rom_t) {
-
+    let names: [UInt32: String] = [
+      GB_BOOT_ROM_DMG0.rawValue: "dmg0_boot",
+      GB_BOOT_ROM_DMG.rawValue: "dmg_boot",
+      GB_BOOT_ROM_MGB.rawValue: "mgb_boot",
+      GB_BOOT_ROM_SGB.rawValue: "sgb_boot",
+      GB_BOOT_ROM_SGB2.rawValue: "sgb2_boot",
+      GB_BOOT_ROM_CGB0.rawValue: "cgb0_boot",
+      GB_BOOT_ROM_CGB.rawValue: "cgb_boot",
+      GB_BOOT_ROM_AGB.rawValue: "agb_boot",
+    ]
+    sameboy.loadBootROM(Bundle.main.path(forResource: names[type.rawValue], ofType: "bin")!)
   }
 }
