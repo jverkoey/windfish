64086214d40c8002eae3d2b03b1b2e8806959dab
Jeff Verkoeyen <jverkoey@gmail.com>
2021-01-21 18:35:04 -0500

    Add a new Cartridge type with tests.
    This new type will allow for a more concise representation of a location in a cartridge and remove much of the boilerplate for converting between banked addresses and cartridge locations.

--- lib/Sources/Windfish/Hardware/Cartridge/Cartridge.swift
+++ lib/Sources/Windfish/Hardware/Cartridge/Cartridge.swift
@@ -60,3 +60,64 @@ extension Cartridge {
     return (address: address, bank: max(1, bank))
   }
 }
+
+extension Cartridge {
+  public final class Location {
+    let address: LR35902.Address
+    let bank: Cartridge.Bank
+
+    lazy var index: Int = {
+      (address < 0x4000)
+        ? Int(truncatingIfNeeded: address)
+        : Int(truncatingIfNeeded: bank) * Int(truncatingIfNeeded: 0x4000) + Int(truncatingIfNeeded: address - 0x4000)
+    }()
+
+    public init(address: LR35902.Address, bank: Cartridge.Bank) {
+      self.address = address
+      self.bank = max(1, bank)
+    }
+
+    public static func + (lhs: Cartridge.Location, rhs: Int) -> Cartridge.Location {
+      return lhs + LR35902.Address(truncatingIfNeeded: rhs)
+    }
+
+    public static func + (lhs: Cartridge.Location, rhs: LR35902.Address) -> Cartridge.Location {
+      return Cartridge.Location(address: lhs.address + rhs, bank: lhs.bank)
+    }
+  }
+}
+
+extension Cartridge.Location: Hashable {
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(bank)
+    hasher.combine(address)
+  }
+}
+
+extension Cartridge.Location: Equatable {
+  public static func == (lhs: Cartridge.Location, rhs: Cartridge.Location) -> Bool {
+    return lhs.index == rhs.index
+  }
+}
+
+extension Cartridge.Location: Comparable {
+  public static func < (lhs: Cartridge.Location, rhs: Cartridge.Location) -> Bool {
+    return lhs.index < rhs.index
+  }
+}
+
+extension Cartridge.Location: Strideable {
+  public func advanced(by n: Int) -> Cartridge.Location {
+    return self + n
+  }
+
+  public func distance(to other: Cartridge.Location) -> Int {
+    return other.index - index
+  }
+}
+
+extension Range where Bound == Cartridge.Location {
+  func asIntRange() -> Range<Int> {
+    return Int(truncatingIfNeeded: lowerBound.index)..<Int(truncatingIfNeeded: upperBound.index)
+  }
+}
