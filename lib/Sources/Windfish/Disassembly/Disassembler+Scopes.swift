24e2cd24c73b1140d1675a986f26fc97352a0343
Jeff Verkoeyen <jverkoey@gmail.com>
2021-01-31 02:18:37 -0500

    Add todo to improve scope disassembler.

--- lib/Sources/Windfish/Disassembly/Disassembler+Scopes.swift
+++ lib/Sources/Windfish/Disassembly/Disassembler+Scopes.swift
@@ -8,6 +8,9 @@ extension Disassembler.BankRouter {
 }
 
 extension Disassembler.BankWorker {
+  // TODO: Explore representing scopes as a dictionary of Address to scope start Address, which in turn can be used
+  // to quickly look up the label name. The methods below are accounting for 1s worth of source generation time.
+
   /** Returns all scopes that intersect with the given location. */
   func contiguousScopes(at location: Cartridge.Location) -> Set<Range<Cartridge.Location>> {
     assert(location.bankIndex == bank)
@@ -19,12 +22,17 @@ extension Disassembler.BankWorker {
   /** Returns all labeled scopes that intersect with the given location. */
   func labeledContiguousScopes(at location: Cartridge.Location) -> [(label: String, scope: Range<Cartridge.Location>)] {
     assert(location.bankIndex == bank)
-    return contiguousScopes(at: location).compactMap { (range: Range<Cartridge.Location>) -> (label: String, scope: Range<Cartridge.Location>)? in
-      guard let label: String = label(at: range.lowerBound) else {
-        return nil
+    var result: [(label: String, scope: Range<Cartridge.Location>)] = []
+    for range: Range<Cartridge.Location> in contiguousScopes {
+      guard range.contains(location) else {
+        continue
       }
-      return (label, range)
+      guard let label: String = label(at: range.lowerBound) else {
+        continue
+      }
+      result.append((label, range))
     }
+    return result
   }
 
   /** Registers a new contiguous scope at the given range. */
