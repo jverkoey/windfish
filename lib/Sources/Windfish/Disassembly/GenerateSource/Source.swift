d888f08196ed44c6c779eba9ffefec376e0ce281
Jeff Verkoeyen <jverkoey@gmail.com>
2021-01-31 19:27:02 -0500

    Pull source out to its own file.

---
+++ lib/Sources/Windfish/Disassembly/GenerateSource/Source.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+extension Disassembler {
+  /** The potential types of source the disassembler can generate. */
+  public struct Source {
+    public enum FileDescription {
+      case charmap(content: String)
+      case datatypes(content: String)
+      case game(content: String)
+      case macros(content: String)
+      case makefile(content: String)
+      case variables(content: String)
+      case bank(number: Cartridge.Bank, content: String, lines: [Line])
+    }
+    public let sources: [String: FileDescription]
+  }
+}
