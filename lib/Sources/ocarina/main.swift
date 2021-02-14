029fde5647d76cf3218ff06ef6ff211a1b780fb5
Jeff Verkoeyen <jverkoey@gmail.com>
2021-02-13 22:02:50 -0500

    Starting to give structure to ocarina.

--- lib/Sources/ocarina/main.swift
+++ lib/Sources/ocarina/main.swift
@@ -4,12 +4,41 @@ import ArgumentParser
 import Windfish
 
 struct Ocarina: ParsableCommand {
-  @Option(help: "The path to the windish project directory.")
+  enum Command: String, Decodable, CaseIterable {
+    case createProject = "create"
+    case disassembleProject = "disassemble"
+  }
+
+  @Argument(help: "Select one of \(Command.allCases.map { $0.rawValue }.joined(separator: ", "))")
+  var commands: [String] = []
+
+  @Option(help: "The path to the Windish project directory.")
   var projectDirectory: String
 
+  var command: Command?
+
+  mutating func validate() throws {
+    guard let commandArgument: String = commands.first else {
+      throw ValidationError("Please specify a command.")
+    }
+    guard let command = Command(rawValue: commandArgument) else {
+      throw ValidationError("\(commandArgument) is not a recognized command.")
+    }
+    self.command = command
+  }
+
   func run() throws {
-    let project = try Project.load(from: URL(fileURLWithPath: projectDirectory))
-    print(project)
+    guard let command = command else {
+      throw ValidationError("Please specify a command.")
+    }
+    switch command {
+    case .createProject:
+      print("Creating new project...")
+
+    case .disassembleProject:
+      let project = try Project.load(from: URL(fileURLWithPath: projectDirectory))
+      print(project)
+    }
   }
 }
 
